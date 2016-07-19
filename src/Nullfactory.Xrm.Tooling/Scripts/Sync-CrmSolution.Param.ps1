acdba9275a4f1cd689898981c0403b089453f842
Shane Carvalho <shanec_@hotmail.com>
2016-07-20 01:02:14 +1000

    Initial-CheckIn
    Initial Check-In

---
+++ src/Nullfactory.Xrm.Tooling/Scripts/Sync-CrmSolution.Param.ps1
@@ -0,0 +1,6 @@
+﻿.\Sync-CrmSolution.ps1 `
+-serverUrl "http://servername/organizationname" `
+-username "domain\username" `
+-password "password" `
+-solutionName "solutionname" `
+-solutionRootFolder "..\..\Demo.Solutions.Primary"
